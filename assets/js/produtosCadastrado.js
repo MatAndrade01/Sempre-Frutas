fec127224d4242b6b1c0aca6e7c4ed34513fdff8
Matandrade01 <ltbmatheus141012@gmail.com>
2024-09-23 17:59:47 -0300

    Começando a tela de produtos cadastrados

--- assets/js/produtosCadastrado.js
+++ assets/js/produtosCadastrado.js
@@ -1,9 +1,14 @@
-//Menu Hamburguer
 
-const menuHamburguer = document.querySelector('#btn')
-const secaoDeItensHamburguer = document.querySelector('.secaoDeItensHamburguer')
-const divMenuHamburguer = document.querySelector('.menuHamburguer')
-menuHamburguer.addEventListener('click',()=>{
-  secaoDeItensHamburguer.classList.toggle('ativo')
-  divMenuHamburguer.classList.toggle('ativo1')
-})
+document.addEventListener('DOMContentLoaded', function() {
+  const button = document.getElementById('idButtonBuguer');
+  const menu = document.getElementById('idDropDowMenu');
+
+  button.addEventListener('click', function() {
+      // Verifica se o menu está visível
+      if (menu.classList.contains('show')) {
+          menu.classList.remove('show'); // Oculta o menu
+      } else {
+          menu.classList.add('show'); // Mostra o menu
+      }
+  });
+});
