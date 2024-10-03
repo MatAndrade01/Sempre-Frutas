3c73f91c6888687ae1b43e5a81fa5b52cf8e66e4
Matheus Andrade <125470760+MatAndrade01@users.noreply.github.com>
2024-10-03 08:29:24 -0300

    Enviando dados de entrada para o Banco de Dados, pagina ainda não concluida

--- assets/js/entradaDeItens.js
+++ assets/js/entradaDeItens.js
@@ -1,3 +1,5 @@
+import { parentPort } from "worker_threads";
+
 document.addEventListener('DOMContentLoaded', function() {
   const button = document.getElementById('idButtonBuguer');
   const menu = document.getElementById('idDropDowMenu');
@@ -11,3 +13,30 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   });
 });
+
+fetch('http://localhost:3333/teste').then(response =>{
+    return response.json()
+}).then(response =>{ console.log(response)})
+
+const formulario = document.getElementById('formEntrada')
+const formData = new FormData(formulario)
+formulario.addEventListener('submit', async()=>{
+    Event.preventDefault()
+  
+    const arrayInput = document.querySelectorAll('.inputEntrada')
+    let nfrecibo = await arrayInput[0].value
+    let quantidade = await arrayInput[5].value
+    let valorcompra = await arrayInput[6].value
+    let nome = await arrayInput[3].value
+    let fornecedor = await arrayInput[4].value
+
+    await fetch('/entradaDeItems',{
+        method:'POST',
+        headers:{
+            "Content-Type": "application/json",
+        },
+        
+    }).then(resolve =>{console.log(resolve)}).catch(error =>{console.log(error)})
+})
+
+
--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,15 @@
 import fastify from 'fastify'
 import { client } from './_database.ts'
 import { z } from 'zod'
-
+import cors from '@fastify/cors'
+import formbody from '@fastify/formbody';
+import multipart from '@fastify/multipart'
 const server = fastify()
+await server.register(cors, { 
+    
+})
+await server.register(formbody);
+
 client.connect()
 server.get('/teste', async(request, response) => {
     const dados = await client.query('SELECT * FROM entrada')
@@ -10,18 +17,20 @@ server.get('/teste', async(request, response) => {
 })
 
 server.post('/entradaDeItems', async (request, reply) => {
+    
+   
     const createEventSchema = z.object({
-        nfrecibo: z.number().nullable(),
-        quantidade: z.number().min(1).nullable(),
-        valorcompra: z.number().min(1).nullable(),
+        nfrecibo: z.string().nullable(),
+        quantidade: z.string().min(1).nullable(),
+        valorcompra: z.string().min(1).nullable(),
         nome: z.string().nullable(),
         fornecedor: z.string().nullable(),
     })
-
-    const data = createEventSchema.parse(request.body)
+    console.log(request.body)
+    const data = await createEventSchema.parse(request.body)
     const result = await client.query('INSERT INTO entrada (nfrecibo,quantidade,valorcompra,nome,fornecedor) VALUES ($1,$2,$3,$4,$5) RETURNING *',
         [data.nfrecibo,data.quantidade,data.valorcompra,data.nome,data.fornecedor])
-    return 'ok'
+    return result.rows
 })
 
 server.listen({port: 3333}).then((response)=>{
